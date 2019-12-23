1cfb46e6a7f88a6923feac2f3e2a2d3d8ff4db79
TysonPham <tysonpham96@gmail.com>
2019-12-23 15:17:59 -0500

    added keydown for piano notes

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const keys = document.querySelectorAll("div.white-key, div.black-key")
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 
+const keys = document.querySelectorAll("div.white-key, div.black-key")
+const whiteKeys = document.querySelectorAll('div.white-key')
+const blackKeys = document.querySelectorAll('div.black-key')
 keys.forEach(key => {
     key.addEventListener('click', () => playNote(key))
 })
 
-function playNote(key){
+document.addEventListener('keydown', e => {
+    const key = e.key
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+    const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+})
+
+function playNote(key) {
     const noteAudio = document.getElementById(key.dataset.note)
     noteAudio.currentTime = 0;
     noteAudio.play()
     key.classList.add('active')
     noteAudio.addEventListener('ended', () => {
-        key.classlist.remove('active')
+        key.classList.remove('active')
     })
 }
